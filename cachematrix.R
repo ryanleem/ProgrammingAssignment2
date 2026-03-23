73464bff57dd1974035b1c2c35e5dca476a55221
Ryan Leem <86704709+ryanleem@users.noreply.github.com>
2026-03-22 18:06:01 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  
+  setmean <- function(solve) {
+    m <<- solve
+  }
+  
+  getmean <- function() {
+    m
+  }
+  
+  list(
+    set = set,
+    get = get,
+    setmean = setmean,
+    getmean = getmean
+  )
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  
+  if (!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmean(m)
+  m
 }
+
+m <- makeCacheMatrix(matrix(c(2, 1, 1, 2), 2, 2))
+cacheSolve(m)
+cacheSolve(m)
